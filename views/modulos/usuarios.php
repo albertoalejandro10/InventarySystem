b08ae41976d898c4543b40ba397b2cd3ed537929
Alberto Alejandro <alberto196g@gmail.com>
2020-07-27 19:35:16 -0400

    Finalizando modulo categorias

--- views/modulos/usuarios.php
+++ views/modulos/usuarios.php
@@ -7,40 +7,118 @@
         </h1>
         <ol class="breadcrumb">
         
-            <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+            <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
             <li class="active">Administrar usuarios</li>
         
         </ol>    
     </section>
 
-    <!-- Main content -->
     <section class="content">
 
-      <!-- Default box -->
       <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
+          <div class="box-header with-border">
+            <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuario">
+              Agregar usuario
+            </button>
           </div>
-        </div>
-        <div class="box-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
+          
+          <div class="box-body">
+            <table class="table table-bordered table-striped">
+              
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Nombre</th>
+                  <th>Usuario</th>
+                  <th>Foto</th>
+                  <th>Perfil</th>
+                  <th>Estado</th>
+                  <th>Último login</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+
+              <tbody>
+              
+                <tr>
+                  <td>1</td>
+                  <td>Usuario Administrador</td>
+                  <td>admin</td>
+                  <td><img src="views/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                  <td>Administrador</td>
+                  <td><button class="btn btn-success btn-xs">Activado</button></td>
+                  <td>2017-12-11 12:05:32</td>
+                  <td>
+                    <div class="btn-group">
+                      <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                    </div>
+                  </td>
+                </tr>
+
+
+                <tr>
+                  <td>1</td>
+                  <td>Usuario Administrador</td>
+                  <td>admin</td>
+                  <td><img src="views/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                  <td>Administrador</td>
+                  <td><button class="btn btn-success btn-xs">Activado</button></td>
+                  <td>2017-12-11 12:05:32</td>
+                  <td>
+                    <div class="btn-group">
+                      <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                    </div>
+                  </td>
+                </tr>
+
+
+                <tr>
+                  <td>1</td>
+                  <td>Usuario Administrador</td>
+                  <td>admin</td>
+                  <td><img src="views/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                  <td>Administrador</td>
+                  <td><button class="btn btn-danger btn-xs">Desactivado</button></td>
+                  <td>2017-12-11 12:05:32</td>
+                  <td>
+                    <div class="btn-group">
+                      <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                    </div>
+                  </td>
+                </tr>
+
+              </tbody>
+
+            </table>
+          </div>
       </div>
-      <!-- /.box -->
 
     </section>
-    <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
+
+  <!-- Modal agregar usuario -->
+
+  <!-- Modal -->
+<div id="modalAgregarUsuario" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
