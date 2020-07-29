fe898e91c5eb6dac804327a9ab35d83df1ec356a
Alberto Alejandro <alberto196g@gmail.com>
2020-07-29 13:53:44 -0400

    Modal para agregar usuarios maquetado

--- views/modulos/usuarios.php
+++ views/modulos/usuarios.php
@@ -48,7 +48,7 @@
                   <td><img src="views/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
                   <td>Administrador</td>
                   <td><button class="btn btn-success btn-xs">Activado</button></td>
-                  <td>2017-12-11 12:05:32</td>
+                  <td>2020-07-29 12:05:32</td>
                   <td>
                     <div class="btn-group">
                       <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
@@ -65,7 +65,7 @@
                   <td><img src="views/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
                   <td>Administrador</td>
                   <td><button class="btn btn-success btn-xs">Activado</button></td>
-                  <td>2017-12-11 12:05:32</td>
+                  <td>2020-07-29 12:05:32</td>
                   <td>
                     <div class="btn-group">
                       <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
@@ -82,7 +82,7 @@
                   <td><img src="views/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
                   <td>Administrador</td>
                   <td><button class="btn btn-danger btn-xs">Desactivado</button></td>
-                  <td>2017-12-11 12:05:32</td>
+                  <td>2020-07-29 12:05:32</td>
                   <td>
                     <div class="btn-group">
                       <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
@@ -104,21 +104,91 @@
 
   <!-- Modal -->
 <div id="modalAgregarUsuario" class="modal fade" role="dialog">
+
   <div class="modal-dialog">
 
     <!-- Modal content-->
     <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
+
+        <form role="form" method="post" enctype="multipart/form-data">
+
+        <div class="modal-header">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        
+          <h4 class="modal-title">Agregar usuarios</h4>
+        
+        </div>
+
+        <!-- Cuerpo del modal -->
+        
+        <div class="modal-body">
+          <div class="box-body">
+
+          <!-- Entrada para el usuario -->
+            <div class="form-group">
+              <div class="input-group">
+
+                  <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                  <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Ingresar nombre" required>
+              </div>
+            </div>
+
+
+            <!-- Entrada para el nombre -->
+            <div class="form-group">
+              <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                  <input type="text" class="form-control input-lg" name="nuevoUsuario" placeholder="Ingresar usuario" required>
+              </div>
+            </div>
+
+
+            <!-- Entrada para la contraseña -->
+            <div class="form-group">
+              <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                  <input type="password" class="form-control input-lg" name="nuevoPassword" placeholder="Ingresar contraseña" required>
+              </div>
+            </div>
+
+
+            <!-- Entrada para seleccionar su perfil -->
+            <div class="form-group">
+              <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-users"></i></span>
+                  <select class="form-control input-lg" name="nuevoPerfil">
+                    <option value="">Seleccionar perfil</option>
+
+                    <option value="Administrador">Administrador</option>
+                    <option value="Especial">Especial</option>
+                    <option value="Vendedor">Vendedor</option>
+                  </select>
+              </div>
+            </div>
+
+            <!-- Entrada para subir foto -->
+            <div class="form-group">
+              <div class="panel">SUBIR FOTO</div>
+              <input type="file" id="nuevaFoto" name="nuevaFoto">
+              <p class="help-block">Peso máximo de la foto 200MB</p>
+              <img src="views/img/usuarios/default/anonymous.png" class="img-thumbnail" width="100px">
+
+            </div>
+
+            
+          </div>
+        </div>
+
+        <!-- Entrada para enviar formulario y pie del modal -->
+        
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+          <button type="submit" class="btn btn-primary">Guardar Usuario</button>
+        </div>
       </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
+
+      </form>
 
   </div>
 </div>
--- views/plantilla.php
+++ views/plantilla.php
@@ -13,6 +13,7 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="icon" href="views/img/plantilla/icono-negro.png">
+  <link rel="stylesheet" href="resources/css/main.css">
 
     <!-- Plugins de CSS -->
 
