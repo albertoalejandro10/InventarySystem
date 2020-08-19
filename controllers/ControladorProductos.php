0a87670ff341cbde7c0dfc65ad6f499407032ec8
Alberto Alejandro <alberto196g@gmail.com>
2020-08-18 20:36:35 -0400

    Mostrar productos

---
+++ controllers/ControladorProductos.php
@@ -0,0 +1,12 @@
+<?php
+
+class ControladorProductos{
+    /* Mostrar productos */
+    static public function ctrMostrarProductos($item, $valor){
+        $tabla = "productos";
+
+        $respuesta = ModeloProductos::mdlMostrarProductos($tabla, $item, $valor);
+
+        return $respuesta;
+    }
+}
--- views/modulos/productos.html.php
+++ views/modulos/productos.html.php
@@ -42,50 +42,70 @@
               </thead>
 
               <tbody>
+              
+                <tr>
+                  <td>1</td>
+                  <td><img src="views/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                  <td>0001</td>
+                  <td>Lorem ipsum dolor sit amet</td>
+                  <td>Lorem Ipsum</td>
+                  <td>20</td>
+                  <td>$ 5.00</td>
+                  <td>$ 10.00</td>
+                  <td>2020-07-29 12:05:32</td>
+                  <td>
 
-              <?php
-
-              $item = null;
-              $valor= null;
-
-              $productos = ControladorProductos::ctrMostrarProductos($item, $valor);
-
-              foreach ($productos as $key => $value) {
-          
-                echo '<tr>
-                        <td>'.($key+1).'</td>
-                        <td><img src="views/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-                        <td>'.$value["codigo"].'</td>
-                        <td>'.$value["descripcion"].'</td>';
-      
-                        $item = "id";
-                        $valor = $value["id_categoria"];
-      
-                        $categoria = ControladorCategorias::ctrMostrarCategorias($item, $valor);
-      
-                       echo '<td>'.$categoria["categoria"].'</td>
-                        <td>'.$value["stock"].'</td>
-                        <td>'.$value["precio_compra"].'</td>
-                        <td>'.$value["precio_venta"].'</td>
-                        <td>'.$value["fecha"].'</td>
-                        <td>
-      
-                          <div class="btn-group">
-                              
-                            <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-      
-                            <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-      
-                          </div>  
-      
-                        </td>
-      
-                      </tr>';
-      
-              }
+                    <div class="btn-group">
+                      <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                    </div>
+                  
+                  </td>
+                </tr>
 
 
-              ?>
+                <tr>
+                  <td>1</td>
+                  <td><img src="views/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                  <td>0001</td>
+                  <td>Lorem ipsum dolor sit amet</td>
+                  <td>Lorem Ipsum</td>
+                  <td>20</td>
+                  <td>$ 5.00</td>
+                  <td>$ 10.00</td>
+                  <td>2020-07-29 12:05:32</td>
+                  <td>
+
+                    <div class="btn-group">
+                      <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                    </div>
+                  
+                  </td>
+                </tr>
+
+
+                <tr>
+                  <td>1</td>
+                  <td><img src="views/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                  <td>0001</td>
+                  <td>Lorem ipsum dolor sit amet</td>
+                  <td>Lorem Ipsum</td>
+                  <td>20</td>
+                  <td>$ 5.00</td>
+                  <td>$ 10.00</td>
+                  <td>2020-07-29 12:05:32</td>
+                  <td>
+
+                    <div class="btn-group">
+                      <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                    </div>
+                  
+                  </td>
+                </tr>
+
+
 
 
               </tbody>
